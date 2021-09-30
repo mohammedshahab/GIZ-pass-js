f63b9fd683676e504e2900ed2bd75b8fa872bfe3
mohammedshahab <iraqapple435@gmail.com>
2021-09-30 22:32:54 +0300

    upload task

--- task.js
+++ task.js
@@ -1,44 +1,55 @@
-let numbers = [
-    5,
-    8,
-    0,
-    1,
-    9,
-    11,
-    15,
-    16
-];
+const numbers =[5, 8, 0, 1, 9, 11, 15, 16];
+console.log("Original numbers list: ", numbers)
+
+// function to Bubble sort (ASC Sorting)
+function ASC_Sort([...newarr]) {
+    for(let i = 0; i < newarr.length; i++)
+    {
+        for(let j = 0; j < newarr.length - 1; j++)
+        {
+            if(newarr[j] > newarr[j + 1]) 
+            {
+                let temp = newarr[j];
+                newarr[j] = newarr[j + 1];
+                newarr[j + 1] = temp;
+            }
+        }
+    }
+    return newarr
+}
+
+console.log("Numbers list After sorting: ", ASC_Sort(numbers))
+
+
+// function to Bubble sort (DESC Sorting)
+function DESC_Sort([...newarr]) {
+    for(let i = 0; i < newarr.length; i++)
+    {
+        for(let j = 0; j < newarr.length - 1; j++)
+        {
+            if(newarr[j] < newarr[j + 1]) 
+            {
+                let temp = newarr[j];
+                newarr[j] = newarr[j + 1];
+                newarr[j + 1] = temp;
+            }
+        }
+    }
+    return newarr
+}
+
+console.log("Numbers list After Desc sorting: ", DESC_Sort(numbers))
 
 console.log("Original numbers list: ", numbers)
 
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        if(numbers[i] > numbers[j + 1])
-        {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
-        }
-    }
+// function to read a data file and sort number and store it in output file
+function Sort_File() {
+    var fs = require('fs');
+    var n = fs.readFileSync('data.txt', 'utf-8');
+    var filenums = n.split(',');
+    filenums = ASC_Sort(filenums);
+    var ASCFile = filenums.join(',');
+    fs.writeFileSync('output.txt', ASCFile);
 }
 
-console.log("Numbers list After sorting: ", numbers)
-
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        if(numbers[i] < numbers[j + 1])
-        {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
-        }
-    }
-}
-
-console.log("Numbers list After Desc sorting: ", numbers)
-
-
+Sort_File();
